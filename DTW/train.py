02e6cce46d11e324e9587be5e2cd37024a289a55
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2019-01-25 17:58:23 +0330

    saving features model with pickle

--- DTW/train.py
+++ DTW/train.py
@@ -4,6 +4,7 @@ from proj_paths import *
 from DTW.extract_features import extract
 
 if __name__ == "__main__":
-    for voice in collect_files(REF_VOICES):
-        _, sig = wav.read(voice)
+    for raw_voice, joined_voice in collect_files(REF_VOICES):
+        _, sig = wav.read(joined_voice)
         feats = extract(sig)
+        save(feats, raw_voice, DTW_MODELS)
--- proj_paths.py
+++ proj_paths.py
@@ -1,3 +1,4 @@
 import os
 os.chdir("/home/mrmirhajian/PycharmProjects/spoken-command-recognition/")
 REF_VOICES = "REF_VOICES/"
+DTW_MODELS = "DTW/Models/"
