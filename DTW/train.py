bd4805f6127a0111605fd6d298a6ec2319352fa6
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2019-01-25 17:30:25 +0330

    extracting voice features(mfcc and delta one & two , zcr, energy)

--- DTW/train.py
+++ DTW/train.py
@@ -1,7 +1,9 @@
 import scipy.io.wavfile as wav
 from utility import *
 from proj_paths import *
+from DTW.extract_features import extract
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     for voice in collect_files(REF_VOICES):
         _, sig = wav.read(voice)
+        feats = extract(sig)
--- proj_paths.py
+++ proj_paths.py
@@ -1,1 +1,3 @@
-REF_VOICES = "./REF_VOICES"
+import os
+os.chdir("/home/mrmirhajian/PycharmProjects/spoken-command-recognition/")
+REF_VOICES = "REF_VOICES/"
